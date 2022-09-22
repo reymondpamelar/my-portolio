341cf53805bf3ca3666fdedea6b11ca62647ff0e
reymondpamelar <reymondpamelar@gmail.com>
2022-09-21 21:10:40 -0700

    vercel fonts fix

--- layouts/dashboard/index.js
+++ layouts/dashboard/index.js
@@ -4,11 +4,6 @@ import ContactBar from "../../components/contactBar";
 export default function DashboardLayout({children}) {
     return (
         <div className={'overflow-x-hidden overflow-y-scroll relative w-screen h-screen bg-mainWhite dark:bg-zinc-800 transition ease-in-out duration-300'}>
-            <Head>
-                <title>Reymond's Portfolio</title>
-                <meta name="description" content="A brief look at my work :)" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
 
             <div className="flex relative h-full">
                 <div id={"scroll"} className={`w-full`}>
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import Navbar from "../components/navbar";
 import {motion} from "framer-motion";
 import Particles from "react-tsparticles";
 import {loadTrianglesPreset} from "tsparticles-preset-triangles";
+import Head from "next/head";
 
 export default function Home() {
 
@@ -32,6 +33,15 @@ export default function Home() {
     }
     return (
         <DashboardLayout>
+            <Head>
+                <title>Reymond's Portfolio</title>
+                <meta name="description" content="A brief look at my work :)" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+            </Head>
+
             <div className={'z-50'}>
                 <Navbar step={step} setStep={setStep}/>
             </div>
