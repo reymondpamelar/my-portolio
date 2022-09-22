89e29b7e79d9095a28f8145f0037f858d37b32d6
reymondpamelar <reymondpamelar@gmail.com>
2022-09-21 23:42:54 -0700

    mobile portrait placement

--- containers/dashboard/about.js
+++ containers/dashboard/about.js
@@ -13,7 +13,12 @@ export default function About() {
                 <h1 className={'text-6xl md:text-8xl pl-4'}>
                     About
                 </h1>
-                <div className={'text-lg mt-32 md:pl-10 pb-10'}>
+                <div className={'text-lg md:mt-32 md:pl-10 pb-10'}>
+                    <div className={'md:hidden h-full flex justify-center mb-6 h-72 p-4'}>
+                        <div className={'drop-shadow-xl inline-block border-b border-zinc-500 overflow-hidden relative'}>
+                            <img src={'portrait-remove-bg.png'} alt={'portrait'} className={'relative z-10 relative '}/>
+                        </div>
+                    </div>
                     <FadeSlide>
                         <div key={'quote1'} className={' grid md:grid-cols-2 mb-20'}>
                             <div className={'space-y-6 p-4'}>
@@ -36,9 +41,9 @@ export default function About() {
                                     However, I do like to explore outside the boundaries of a browser.
                                 </div>
                             </div>
-                            <div className={'h-full flex justify-center '}>
+                            <div className={'hidden h-full md:flex justify-center '}>
                                 <div className={'drop-shadow-xl inline-block border-b border-zinc-500 overflow-hidden relative bottom-32'}>
-                                    <img src={'portrait-remove-bg.png'} alt={'portrait'} className={'relative z-10 relative -bottom-32 animate__animated animate__fadeInUp'}/>
+                                    <img src={'portrait-remove-bg.png'} alt={'portrait'} className={'relative z-10 relative -bottom-32 '}/>
                                 </div>
                             </div>
                         </div>
