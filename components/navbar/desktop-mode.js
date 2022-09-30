5fdace4c49ca6cf851cc3ac5f603c8dc1515dacf
reymondpamelar <reymondpamelar@gmail.com>
2022-09-30 00:12:30 -0700

    Nav scroll

--- components/navbar/desktop-mode.js
+++ components/navbar/desktop-mode.js
@@ -1,10 +1,8 @@
 import * as Icon from 'react-feather'
 import DarkModeToggle from "../dark-mode/toggle";
-import {useEffect} from "react";
-import FadeSlide from "../flip-move/fadeSlide";
-import BorderSlideButton from "../general-ui/buttons/border-slide-button";
+import {useEffect, useState} from "react";
 import React from "react";
-import AccordianVertical from "../flip-move/AccordionVertical";
+import AccordionVertical from "../flip-move/AccordionVertical";
 
 export default function DesktopNavbar(props) {
 
@@ -22,6 +20,37 @@ export default function DesktopNavbar(props) {
         }
         document.getElementById('main').addEventListener('scroll', addNavBg, false)
     },[])
+
+    function scrollTo(element){
+        document.getElementById(element).scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    }
+
+    let [currUnderline, setCurrUnderline] = useState('Home')
+    useEffect(()=>{
+        function underlineElement(){
+            var currPos = 0
+            let homePos = document.getElementById('Home').getBoundingClientRect().top;
+            let aboutPos = document.getElementById('About').getBoundingClientRect().top;
+            let projectsPos = document.getElementById('Projects').getBoundingClientRect().top;
+            let positions = [homePos, aboutPos, projectsPos]
+            let closest = positions.reduce(function(prev, curr) {
+                return (Math.abs(curr - currPos) < Math.abs(prev - currPos) ? curr : prev);
+            });
+            switch(positions.indexOf(closest)){
+                case 0:
+                    setCurrUnderline('Home')
+                    break;
+                case 1:
+                    setCurrUnderline('About')
+                    break;
+                case 2:
+                    setCurrUnderline('Projects')
+                    break;
+            }
+        }
+        document.getElementById('main').addEventListener('scroll', underlineElement, false)
+
+    },[])
     return (
         <div className={'flex justify-center w-full fixed top-0 z-50'}>
             <div id={'nav'}  className={'bg-gradient-to-t from-transparent to-mainWhite dark:to-zinc-900 transition ease-in-out duration-500 h-full w-full absolute'}>
@@ -29,26 +58,26 @@ export default function DesktopNavbar(props) {
             <div className={'grid grid-cols-9 relative w-full'}>
                 <h4 className={'p-5 pl-12 col-span-2'}>RP</h4>
                 <div className={'col-span-5'}>
-                    <AccordianVertical delay={3000} useOnScreen={false} className={'z-50 flex justify-center items-center gap-12 w-full p-5 px-12 relative'}>
-                        <div onClick={()=>props.setStep(0)} className={'cursor-pointer p-2 overflow-hidden relative font-asap tracking-widest'}>
+                    <AccordionVertical delay={3000} useOnScreen={false} className={'z-50 flex justify-center items-center gap-12 w-full p-5 px-12 relative'}>
+                        <div onClick={()=> scrollTo('Home')} className={'cursor-pointer p-2 overflow-hidden relative font-asap tracking-widest'}>
                             <div>
                                 / HOME
                             </div>
-                            <div className={props.step !== 0 ? 'translate-x-40 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500' : 'translate-x-0 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500'}></div>
+                            <div className={currUnderline !== 'Home' ? 'translate-x-40 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500' : 'translate-x-0 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500'}></div>
                         </div>
-                        <div onClick={()=>props.setStep(1)} className={'cursor-pointer p-2 overflow-hidden relative font-asap tracking-widest'}>
+                        <div onClick={()=> scrollTo('About')} className={'cursor-pointer p-2 overflow-hidden relative font-asap tracking-widest'}>
                             <div>
                                 / ABOUT
                             </div>
-                            <div className={props.step !== 1 ? 'translate-x-40 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500' : 'translate-x-0 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500'}></div>
+                            <div className={currUnderline !== 'About' ? 'translate-x-40 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500' : 'translate-x-0 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500'}></div>
                         </div>
-                        <div onClick={()=>props.setStep(2)} className={'cursor-pointer p-2 overflow-hidden relative font-asap tracking-widest'}>
+                        <div onClick={()=> scrollTo('Projects')} className={'cursor-pointer p-2 overflow-hidden relative font-asap tracking-widest'}>
                             <div>
                                 / PROJECTS
                             </div>
-                            <div className={props.step !== 2 ? 'translate-x-40 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500' : 'translate-x-0 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500'}></div>
+                            <div className={currUnderline !== 'Projects' ? 'translate-x-40 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500' : 'translate-x-0 absolute inset-0 bottom-0 border-b border-zinc-500 dark:border-white transition ease-in-out duration-500'}></div>
                         </div>
-                    </AccordianVertical>
+                    </AccordionVertical>
                 </div>
                 <div className={'flex justify-end items-center p-5 pr-12 gap-8 col-span-2'}>
                     <div className={'cursor-pointer flex items-center gap-2'}>
