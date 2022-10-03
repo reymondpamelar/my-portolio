c7405c433f50e922e021a5070ed03be4521c8e9d
reymondpamelar <reymondpamelar@gmail.com>
2022-10-03 00:11:07 -0700

    favicon adjustments

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
                     rel="stylesheet"
                 />
+                <Favicon/>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'}/>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
